5392cc63909680eb9fc112e94b48cff1278e6d91
euisungkang <euisungkang@gmail.com>
2023-01-06 13:20:05 +0000

    1: Two Sum. Runtime O(n), utilizes ES6 Hash

--- 1.two-sum.js
+++ 1.two-sum.js
@@ -11,7 +11,28 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    
+    let hash = new Map()
+
+    for (let i = 0; i < nums.length; i++) {
+        if (hash.has(target - nums[i])) {
+            return [hash.get(target - nums[i]), i]
+        } else {
+            hash.set(nums[i], i)
+        }
+    }
+
+    return []
 };
 // @lc code=end
 
+let assert = require('assert')
+
+function main() {
+    assert.deepEqual(twoSum([2,7,11,15], 9), [0,1])
+    assert.deepEqual(twoSum([3,2,4], 6), [1,2])
+    assert.deepEqual(twoSum([3,3,], 6), [0,1])
+}
+
+if (require.main === module) {
+    main()
+}
